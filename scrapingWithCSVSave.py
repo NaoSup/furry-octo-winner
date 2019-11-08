fc78f2262356b2fd37ce27d51b791a38dc366fca
Salhotra Dass <dinesh.salhotradass@ynov.com>
2019-11-08 22:22:27 +0100

    fix label problem

--- scrapingWithCSVSave.py
+++ scrapingWithCSVSave.py
@@ -5,8 +5,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
+import os
 import csv
 
+try:
+    os.remove('Scraping data et Analytique.csv')
+except OSError:
+    pass
 
 driver = webdriver.Chrome(
     '../chromedriver')
@@ -27,25 +32,35 @@ list_results = wait.until(
     EC.presence_of_element_located((By.XPATH, '//ul[@id="milieu"]')))
 options = list_results.find_elements_by_tag_name("li")
 links = []
+investisseursName = []
+# get labels first
+
 for option in options:
     links.append(option.find_element_by_xpath(
         './/h6/a').get_attribute('href'))
-for link in links:
+    investisseursName.append(option.find_element_by_xpath(
+        './/h6/a').text)
+driver.get(links[0])
+starUpTab = wait.until(
+    EC.presence_of_element_located((By.ID, 'example')))
 
+starUpTabElementsLabels = starUpTab.find_element_by_tag_name('thead')
+starUpListLabelsrow = starUpTabElementsLabels.find_element_by_tag_name(
+    'tr')
+starUpListLabels = starUpListLabelsrow.find_elements_by_tag_name('th')
+lables = []
+for starUpLabel in starUpListLabels:
+    lables.append(starUpLabel.text)
+lables.append('Investisseurs')
+with open('Scraping data et Analytique.csv', 'a+') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(lables)
+
+for i, link in enumerate(links):
     driver.get(link)
     starUpTab = wait.until(
         EC.presence_of_element_located((By.ID, 'example')))
 
-    starUpTabElementsLabels = starUpTab.find_element_by_tag_name('thead')
-    starUpListLabelsrow = starUpTabElementsLabels.find_element_by_tag_name(
-        'tr')
-    starUpListLabels = starUpListLabelsrow.find_elements_by_tag_name('th')
-    lables = []
-    for starUpLabel in starUpListLabels:
-        lables.append(starUpLabel.text)
-    with open('Scraping data et Analytique.csv', 'a+') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(lables)
     starUpTabElements = starUpTab.find_element_by_tag_name('tbody')
     starUpList = starUpTabElements.find_elements_by_tag_name('tr')
 
@@ -58,6 +73,7 @@ for link in links:
             '//td/span').text)
         startUpInfo.append(starUpTabElement.find_element_by_xpath(
             '//td[3]').text)
+        startUpInfo.append(investisseursName[i])
         with open('Scraping data et Analytique.csv', 'a+') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(startUpInfo)
