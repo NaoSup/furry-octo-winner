34b5d86d118040c41647a6c242f1922f54c12c3c
Salhotra Dass <dinesh.salhotradass@ynov.com>
2019-11-08 16:56:52 +0100

    logintoWebSite

--- scraping.py
+++ scraping.py
@@ -5,5 +5,25 @@ import pandas as pd
 
 
 driver = webdriver.Chrome(
-    '/Users/dineshsalhotra/Documents/Formations/Automatisation Informatique/furry-octo-winner')
-driver.get('https://www.myfrenchstartup.com/fr/')
+    '/Users/dineshsalhotra/Documents/Formations/Automatisation Informatique/furry-octo-winner/chromedriver')
+driver.get('https://www.myfrenchstartup.com/fr/connexion')
+
+
+driver.find_element_by_name('login_connect').send_keys(
+    'capikif560@mailhub24.com')
+driver.find_element_by_name('pwd').send_keys('E7qt4Sqg@np3MW4')
+driver.find_element_by_xpath('//button[@type=\'submit\']').click()
+driver.get(
+    'https://www.myfrenchstartup.com/fr/liste-investisseurs')
+
+
+list3 = driver.find_element_by_xpath('//div[@id=\'bloc_resultat\']//ul')
+
+
+# items = list3.find_elements_by_tag_name("li")
+# for item in items:
+#     item
+#     print(item)
+# # list = driver.find_element_by_xpath(
+# #     '//ul[@id=\'milieu\']')
+# driver.close()
